7ac5c7e314b7bbec51a22fbc69a4af997f641703
USER <USER@122.44.176.226>
2019-09-15 13:11:50 +0900

    백준1

--- src/baekjoon1.java
+++ src/baekjoon1.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
+//น้มุ 10951น๘ A+B-4
 
 public class baekjoon1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner(System.in);
+		while(s.hasNextInt()) {
+			int a = s.nextInt();
+			int b = s.nextInt();
+			System.out.println(a+b);
+		}
 	}
 
 }
