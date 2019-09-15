e296aa3e3edea36a2142efbaf5cbf0069a97e7d1
USER <USER@122.44.176.226>
2019-09-15 12:45:21 +0900

    commit

--- src/Hello2.java
+++ src/Hello2.java
@@ -3,7 +3,9 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1;
+		System.out.println("a");
+		System.out.println(a);
 	}
 
 }
