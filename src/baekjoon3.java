c82de4fe26003e62a0c0a8b33d8d5b12e1d1037d
USER <USER@122.44.176.226>
2019-09-22 13:57:26 +0900

    백준 10718번

--- src/baekjoon3.java
+++ src/baekjoon3.java
@@ -3,7 +3,7 @@ public class baekjoon3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 
 	}
 
---
+++ src/baekjoon4.java
@@ -0,0 +1,9 @@
+
+public class baekjoon4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("강한친구 대한육군\n" + "강한친구 대한육군");
+	}
+
+}
