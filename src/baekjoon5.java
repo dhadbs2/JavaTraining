2307535cfc0311e70a3feb95ffcce183de7ced93
USER <USER@122.44.176.226>
2019-10-05 12:01:48 +0900

    백준 10171

---
+++ src/baekjoon5.java
@@ -0,0 +1,9 @@
+
+public class baekjoon5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	}
+
+}
