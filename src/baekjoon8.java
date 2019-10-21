9b495ac6b4c841445f9d60c8ff2e3816a02c2467
USER <USER@122.44.176.226>
2019-10-21 23:24:18 +0900

    10869문제

---
+++ src/baekjoon8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//10869¹®Á¦
+public class baekjoon8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    //sc.close();
+	 
+	    System.out.println(a + b);
+	    System.out.println(a - b);
+	    System.out.println(a * b);
+	    System.out.println(a / b);     
+	    System.out.println(a % b);
+	}
+
+}
